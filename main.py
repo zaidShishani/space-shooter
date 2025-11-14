5748c8ed7ff33abf13fc571f46f50a6f25ee55e1
zaidShishani <zaidjamo94@Gmail.com>
2025-11-14 12:11:01 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame 
+from pygame.locals import *
+import constants
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
